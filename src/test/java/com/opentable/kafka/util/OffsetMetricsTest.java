e2012f6cad45f9c19bf6d566f719d255c29dec60
Chris Pennello <cpennello@opentable.com>
2018-02-06 14:58:35 -0800

    Updates read/write to be per-test.
    Jira: OTPL-2035
    
    Instead of rule.  This is because we'll want a multi-partition test with
    some of the same harness code.

--- src/test/java/com/opentable/kafka/util/OffsetMetricsTest.java
+++ src/test/java/com/opentable/kafka/util/OffsetMetricsTest.java
@@ -5,37 +5,39 @@ import java.time.Duration;
 import com.codahale.metrics.Counting;
 import com.codahale.metrics.MetricRegistry;
 
-import org.junit.Rule;
 import org.junit.Test;
 
 public class OffsetMetricsTest {
     private static final String METRIC_NS = "foobar";
 
-    @Rule
-    public final ReadWriteRule rw = new ReadWriteRule();
-
     @Test(timeout = 30_000, expected = IllegalArgumentException.class)
     public void testNoTopics() {
-        OffsetMetrics
-                .builder(METRIC_NS, new MetricRegistry(), rw.getGroupId(), rw.getBroker().getKafkaBrokerConnect())
-                .build();
+        try (ReadWriteRule rw = new ReadWriteRule()) {
+            OffsetMetrics
+                    .builder(METRIC_NS, new MetricRegistry(), rw.getGroupId(), rw.getBroker().getKafkaBrokerConnect())
+                    .build();
+        }
     }
 
     @Test(timeout = 30_000, expected = IllegalArgumentException.class)
     public void testMissingTopic() {
-        OffsetMetrics
-                .builder(METRIC_NS, new MetricRegistry(), rw.getGroupId(), rw.getBroker().getKafkaBrokerConnect())
-                .addTopics("no-topic-1")
-                .build();
+        try (ReadWriteRule rw = new ReadWriteRule()) {
+            OffsetMetrics
+                    .builder(METRIC_NS, new MetricRegistry(), rw.getGroupId(), rw.getBroker().getKafkaBrokerConnect())
+                    .addTopics("no-topic-1")
+                    .build();
+        }
     }
 
     @Test(timeout = 60_000)
     public void testMetrics() throws InterruptedException {
-        try (OffsetMetrics metrics = OffsetMetrics
-                .builder(METRIC_NS, new MetricRegistry(), rw.getGroupId(), rw.getBroker().getKafkaBrokerConnect())
-                .addTopic(rw.getTopicName())
-                .withPollPeriod(Duration.ofMillis(500))
-                .build()
+        try (
+                ReadWriteRule rw = new ReadWriteRule();
+                OffsetMetrics metrics = OffsetMetrics
+                        .builder(METRIC_NS, new MetricRegistry(), rw.getGroupId(), rw.getBroker().getKafkaBrokerConnect())
+                        .addTopic(rw.getTopicName())
+                        .withPollPeriod(Duration.ofMillis(500))
+                        .build()
         ) {
             metrics.start();
 
@@ -45,29 +47,33 @@ public class OffsetMetricsTest {
             rw.readTestRecords(n);
 
             // Ensure metrics true with records written and read.
-            waitForMetric(metrics, "size", n);
-            waitForMetric(metrics, "offset", n);
-            waitForMetric(metrics, "lag", 0);
+            waitForMetric(rw, metrics, "size", n);
+            waitForMetric(rw, metrics, "offset", n);
+            waitForMetric(rw, metrics, "lag", 0);
 
             // Write some more, but don't read yet.
             final int more = 3;
             rw.writeTestRecords(n + 1, n + more);
 
             // Ensure metrics true up, including lag.
-            waitForMetric(metrics, "size", n + more);
-            waitForMetric(metrics, "lag", more);
-            waitForMetric(metrics, "offset", n);
+            waitForMetric(rw, metrics, "size", n + more);
+            waitForMetric(rw, metrics, "lag", more);
+            waitForMetric(rw, metrics, "offset", n);
 
             // Have consumer catch up.
             rw.readTestRecords(more);
 
             // Make sure lag goes back to zero and offset catches up.
-            waitForMetric(metrics, "lag", 0);
-            waitForMetric(metrics, "offset", n + more);
+            waitForMetric(rw, metrics, "lag", 0);
+            waitForMetric(rw, metrics, "offset", n + more);
         }
     }
 
-    private void waitForMetric(final OffsetMetrics metrics, final String nameSuffix, final long value)
+    private void waitForMetric(
+            final ReadWriteRule rw,
+            final OffsetMetrics metrics,
+            final String nameSuffix,
+            final long value)
             throws InterruptedException {
         final String metricName = String.format("%s.%s.partition.0.%s", METRIC_NS, rw.getTopicName(), nameSuffix);
         while (true) {
