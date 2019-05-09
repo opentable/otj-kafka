8749d219e62994aaa9d76f48d0219a8c13e2f732
Michael Bell <mbell@opentable.com>
2019-05-09 10:24:06 -0700

    Just a sketch of basic streams props support

--- otj-kafka/src/main/java/com/opentable/kafka/builders/KafkaStreamsPropertiesBuilder.java
+++ otj-kafka/src/main/java/com/opentable/kafka/builders/KafkaStreamsPropertiesBuilder.java
@@ -1,12 +1,14 @@
 package com.opentable.kafka.builders;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 import javax.inject.Inject;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.streams.StreamsConfig;
 
 /**
  * Streams isn't really possible to build a builder for, but we can preconfigure some of the options.
@@ -21,12 +23,12 @@ public class KafkaStreamsPropertiesBuilder {
         this.kafkaBuilderFactoryBean = kafkaBuilderFactoryBean;
     }
 
-    public Properties getStreamsProperties(String name) {
-        return getStreamsProperties(name, false, false);
+    public Properties getStreamsProperties(String name, List<String> bootstrapServers) {
+        return getStreamsProperties(name, false, false, bootstrapServers);
     }
 
-    //TODO: probably should be fronted with builder for StreamsConfig esp applicationId, bootstrap
-    public Properties getStreamsProperties(String name, boolean disableMetrics, boolean disableLogging) {
+    //TODO: probably should be fronted with builder for StreamsConfig, this is a bit ugly.
+    public Properties getStreamsProperties(String name, boolean disableMetrics, boolean disableLogging, List<String> bootStrapServers) {
         // Get the merged namespaced properties for consumer and producer specific
         final KafkaConsumerBuilder<?,?> kafkaConsumerBuilder = this.kafkaBuilderFactoryBean.consumerBuilder(name);
         final KafkaProducerBuilder<?,?> kafkaProducerBuilder = this.kafkaBuilderFactoryBean.producerBuilder(name);
@@ -38,6 +40,7 @@ public class KafkaStreamsPropertiesBuilder {
             kafkaConsumerBuilder.disableLogging();
             kafkaProducerBuilder.disableLogging();
         }
+        kafkaConsumerBuilder.withBootstrapServers(bootStrapServers);
         final Map<String, Object> consumerStreamsProperties = kafkaConsumerBuilder.buildProperties();
         final Map<String, Object> producerStreamProperties = kafkaProducerBuilder.buildProperties();
         // Optional: Get Streams specific from properties?
@@ -46,6 +49,7 @@ public class KafkaStreamsPropertiesBuilder {
         mergedMap.putAll(producerStreamProperties);
         // Streams doesn't allow this according to javadoc
         mergedMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        mergedMap.put(StreamsConfig.APPLICATION_ID_CONFIG, name);
         return toProperties(mergedMap);
     }
 
