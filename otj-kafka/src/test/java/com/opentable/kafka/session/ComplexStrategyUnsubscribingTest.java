42b80106589cc932a0c66006ca9b582acb47c740
Lu Tahmazyan <ltahmazyan@opentable.com>
2022-04-14 14:23:34 -0700

    increase test timeouts

--- otj-kafka/src/test/java/com/opentable/kafka/session/ComplexStrategyUnsubscribingTest.java
+++ otj-kafka/src/test/java/com/opentable/kafka/session/ComplexStrategyUnsubscribingTest.java
@@ -23,7 +23,7 @@ public class ComplexStrategyUnsubscribingTest extends AbstractUnsubscribingTest 
 
     // Same as the simple test, but with 3 consumers
     // Total messages remains bounded, and no sleep, so under normal circumstances no rebalance
-    @Test(timeout = 35000)
+    @Test(timeout = 60000)
     public void testComplexWithStrategy() throws ExecutionException, InterruptedException {
         performComplexTestWithStrategy();
     }
